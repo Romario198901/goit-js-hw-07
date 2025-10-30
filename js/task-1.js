7dd25f9788870ee66d30bde81fb7ae044c79ca5a
Roman Hrydin <romario198901@gmail.com>
2025-10-30 19:58:06 +0200

    format files

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,9 @@
-const categories = document.querySelectorAll('.item'); 
+const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 
-categories.forEach(category => {
-  const title = category.querySelector('.item-title').textContent;
-  const elements = category.querySelectorAll('.item-list-name'); 
+categories.forEach((category) => {
+  const title = category.querySelector(".item-title").textContent;
+  const elements = category.querySelectorAll(".item-list-name");
   console.log(`Category: ${title}`);
   console.log(`Elements: ${elements.length}`);
 });
-
