f1887822965e25d36c0dd949a163f159c1cb7f0a
Roman Hrydin <romario198901@gmail.com>
2025-10-31 17:21:21 +0200

    complete task5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const refs = {
+  body: document.querySelector("body"),
+  currentColor: document.querySelector(".color"),
+  buttonChangeColor: document.querySelector(".change-color"),
+};
+refs.buttonChangeColor.addEventListener("click", handleButtonClicked);
+function handleButtonClicked() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.currentColor.textContent = refs.body.style.backgroundColor;
+  return refs.currentColor.textContent;
+}
