25857954617c11d8c1c638fd96724f3564311444
Roman Hrydin <romario198901@gmail.com>
2025-10-31 11:29:54 +0200

    complete task2

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,15 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+function imageTemplate(obj) {
+  return `<li class="gallery-item"><img src=${obj.url} alt=${obj.alt}></li>`;
+}
+function imagesTemplate(arr) {
+  const markup = arr.map(imageTemplate).join("");
+  return markup;
+}
+const gallery = document.querySelector(".gallery");
+const markupOfGallery = imagesTemplate(images);
+gallery.insertAdjacentHTML("afterbegin", markupOfGallery);
