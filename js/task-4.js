f5d0d93458d67a69239437aecdc7d9b91bc18309
Roman Hrydin <romario198901@gmail.com>
2025-10-31 13:56:29 +0200

    complete task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,16 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (
+    form.elements.email.value.trim() === "" ||
+    form.elements.password.value.trim() === ""
+  ) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const userData = {};
+  userData.email = form.elements.email.value.trim();
+  userData.password = form.elements.password.value.trim();
+  console.log(userData);
+  form.reset();
+});
